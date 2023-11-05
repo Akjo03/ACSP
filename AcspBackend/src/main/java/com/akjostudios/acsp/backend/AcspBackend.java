37429fb5a0561da4ebe35dae641354a024bf8dd1
Lukas Küffer <lukas.kueffer@outlook.com>
2023-11-05 18:42:48 +0100

    Added backend service

---
+++ AcspBackend/src/main/java/com/akjostudios/acsp/backend/AcspBackend.java
@@ -0,0 +1,24 @@
+package com.akjostudios.acsp.backend;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@RequiredArgsConstructor
+@Getter
+@Log4j2
+public class AcspBackend {
+    private final ApplicationContext context;
+    private final ApplicationArguments args;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcspBackend.class, args);
+    }
+}
