60150ef401ba6c87f71204c5daeadc7c49351d0a
Lukas Küffer <lukas.kueffer@outlook.com>
2023-11-07 22:50:17 +0100

    Improved health indicator of bot to combine both checks correctly

--- AcspBot/src/main/java/com/akjostudios/acsp/bot/indicators/ReadinessIndicator.java
+++ AcspBot/src/main/java/com/akjostudios/acsp/bot/indicators/ReadinessIndicator.java
@@ -10,7 +10,11 @@ import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
@@ -20,19 +24,18 @@ public class ReadinessIndicator implements ReactiveHealthIndicator {
     private final WebClient backendClient;
 
     public Mono<Health> health() {
-        return Mono.zip(
+        return Flux.merge(
                 checkBackendService(),
                 checkBotReady()
-        ).map(tuple -> {
-            Health.Builder builder = Health.up();
-            builder = tuple.getT1().getStatus().equals(Status.DOWN)
-                    ? builder.down().withDetail("backendService", tuple.getT1().getStatus().toString() + " " + tuple.getT1().getDetails().toString())
-                    : builder.up();
-            builder = tuple.getT2().getStatus().equals(Status.DOWN)
-                    ? builder.down().withDetail("discordConnection", tuple.getT2().getStatus().toString() + " " + tuple.getT2().getDetails().toString())
-                    : builder.up();
-            return builder.build();
-        });
+        ).collectList().map(healths -> healths.stream()
+                .allMatch(health -> health.getStatus().equals(Status.UP))
+                ? Health.up().build()
+                : Health.down().withDetails(healths.stream()
+                        .filter(health -> !health.getStatus().equals(Status.UP))
+                        .flatMap(health -> health.getDetails().entrySet().stream())
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                ).build()
+        );
     }
 
     private Mono<Health> checkBackendService() {
