be3f4f1de30a7bd06838e42d22157038413e63d5
Lukas Küffer <lukas.kueffer@outlook.com>
2023-11-07 21:31:24 +0100

    Some very minor quality of life changes to the code

--- AcspBackend/src/main/java/com/akjostudios/acsp/backend/config/WebClientConfig.java
+++ AcspBackend/src/main/java/com/akjostudios/acsp/backend/config/WebClientConfig.java
@@ -2,7 +2,6 @@ package com.akjostudios.acsp.backend.config;
 
 import com.akjostudios.acsp.backend.properties.ExternalServiceProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
--- AcspBackend/src/main/java/com/akjostudios/acsp/backend/indicators/ReadinessIndicator.java
+++ AcspBackend/src/main/java/com/akjostudios/acsp/backend/indicators/ReadinessIndicator.java
@@ -1,19 +1,21 @@
 package com.akjostudios.acsp.backend.indicators;
 
-import com.akjostudios.acsp.backend.config.WebClientConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class ReadinessIndicator implements ReactiveHealthIndicator {
-    private final WebClientConfig serviceClients;
+    @Qualifier("client.service.bot")
+    private final WebClient botClient;
 
     public Mono<Health> health() {
         return checkBotService();
