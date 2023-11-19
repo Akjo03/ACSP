7697b4e3aab8e79b78e6d1e9c9807f34341b344e
Lukas Küffer <lukas.kueffer@outlook.com>
2023-11-19 21:40:44 +0100

    Added metrics for exporting health information to backend

---
+++ AcspBackend/src/main/java/com/akjostudios/acsp/backend/config/HealthMetricExportConfig.java
@@ -0,0 +1,31 @@
+package com.akjostudios.acsp.backend.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.actuate.health.HealthContributorRegistry;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class HealthMetricExportConfig {
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> healthRegistryCustomizer(@NotNull HealthContributorRegistry healthRegistry) {
+        return registry -> healthRegistry.stream().forEach(
+            healthContributor -> registry
+                .gauge("health", Tags.of("name", healthContributor.getName()), healthRegistry, health -> getStatusCode(
+                        ((HealthIndicator) health.getContributor(healthContributor.getName())).getHealth(false).getStatus()
+                ))
+        );
+    }
+
+    private int getStatusCode(@NotNull Status status) {
+        if (status.equals(Status.UP)) return 1;
+        if (status.equals(Status.OUT_OF_SERVICE)) return -1;
+        if (status.equals(Status.DOWN)) return 0;
+        return -2;
+    }
+}
