4e220edf38284c6910598a87a1c362105401a7bc
Lukas Küffer <lukas.kueffer@outlook.com>
2023-11-20 22:10:18 +0100

    Added discord primitives service and expanded AcspBot API

--- AcspBot/src/main/java/com/akjostudios/acsp/bot/discord/api/AcspBot.java
+++ AcspBot/src/main/java/com/akjostudios/acsp/bot/discord/api/AcspBot.java
@@ -1,14 +1,27 @@
 package com.akjostudios.acsp.bot.discord.api;
 
+import com.akjostudios.acsp.bot.discord.common.primitive.AcspDiscordChannelCategories;
+import com.akjostudios.acsp.bot.discord.common.primitive.AcspDiscordChannels;
+import com.akjostudios.acsp.bot.discord.common.primitive.AcspDiscordRoles;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 @Component
 @SuppressWarnings("unused")
 public interface AcspBot {
+    void useChannel(@NotNull AcspDiscordChannels channel, @NotNull Consumer<? super Channel> consumer);
+    void useRole(@NotNull AcspDiscordRoles role, @NotNull Consumer<Role> consumer);
+    void useCategory(@NotNull AcspDiscordChannelCategories category, @NotNull Consumer<Channel> consumer);
+    void useChannelsInCategory(@NotNull AcspDiscordChannelCategories category, @NotNull Consumer<List<GuildChannel>> consumer);
     @NotNull JDA.Status getStatus();
-    void shutdown(ConfigurableApplicationContext context, int delay);
-    void restart(ConfigurableApplicationContext context, int delay);
+    void shutdown(@NotNull ConfigurableApplicationContext context, int delay);
+    void restart(@NotNull ConfigurableApplicationContext context, int delay);
 }
--- AcspBot/src/main/java/com/akjostudios/acsp/bot/discord/impl/AcspBotImpl.java
+++ AcspBot/src/main/java/com/akjostudios/acsp/bot/discord/impl/AcspBotImpl.java
@@ -4,11 +4,18 @@ import com.akjostudios.acsp.bot.AcspBotApp;
 import com.akjostudios.acsp.bot.discord.api.AcspBot;
 import com.akjostudios.acsp.bot.discord.common.BotEnvironment;
 import com.akjostudios.acsp.bot.discord.common.listener.CommonListener;
+import com.akjostudios.acsp.bot.discord.common.primitive.AcspDiscordChannelCategories;
+import com.akjostudios.acsp.bot.discord.common.primitive.AcspDiscordChannels;
+import com.akjostudios.acsp.bot.discord.common.primitive.AcspDiscordRoles;
+import com.akjostudios.acsp.bot.discord.service.DiscordPrimitiveService;
 import com.akjostudios.acsp.bot.properties.BotConfigProperties;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import org.jetbrains.annotations.NotNull;
@@ -19,15 +26,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 @Component
 @Slf4j
 public class AcspBotImpl implements AcspBot {
     @Getter private static BotEnvironment environment;
 
+    private final DiscordPrimitiveService primitiveService;
+
     private final JDA botInstance;
 
     @Autowired
-    public AcspBotImpl(@NotNull ApplicationContext context, @NotNull BotConfigProperties properties) {
+    public AcspBotImpl(
+            @NotNull ApplicationContext context,
+            @NotNull BotConfigProperties properties,
+            @NotNull DiscordPrimitiveService primitiveService
+    ) {
         environment = properties.getEnvironment();
         log.info("Starting ACSP Discord Bot in {} environment...", environment.name());
 
@@ -38,6 +54,28 @@ public class AcspBotImpl implements AcspBot {
         builder.addEventListeners(context.getBean(CommonListener.class));
 
         botInstance = builder.build();
+
+        this.primitiveService = primitiveService;
+    }
+
+    @Override
+    public void useChannel(@NotNull AcspDiscordChannels channel, @NotNull Consumer<? super Channel> consumer) {
+        primitiveService.getChannel(botInstance, channel).ifPresent(consumer);
+    }
+
+    @Override
+    public void useRole(@NotNull AcspDiscordRoles role, @NotNull Consumer<Role> consumer) {
+        primitiveService.getRole(botInstance, role).ifPresent(consumer);
+    }
+
+    @Override
+    public void useCategory(@NotNull AcspDiscordChannelCategories category, @NotNull Consumer<Channel> consumer) {
+        primitiveService.getCategory(botInstance, category).ifPresent(consumer);
+    }
+
+    @Override
+    public void useChannelsInCategory(@NotNull AcspDiscordChannelCategories category, @NotNull Consumer<List<GuildChannel>> consumer) {
+        primitiveService.getChannelsInCategory(botInstance, category).ifPresent(consumer);
     }
 
     @Override
@@ -46,7 +84,7 @@ public class AcspBotImpl implements AcspBot {
     }
 
     @Override
-    public void shutdown(ConfigurableApplicationContext context, int delay) {
+    public void shutdown(@NotNull ConfigurableApplicationContext context, int delay) {
         boolean shutdownFailed = false;
 
         try (context) { botInstance.shutdownNow(); } catch (Exception ignored) {
@@ -57,7 +95,7 @@ public class AcspBotImpl implements AcspBot {
     }
 
     @Override
-    public void restart(ConfigurableApplicationContext context, int delay) {
+    public void restart(@NotNull ConfigurableApplicationContext context, int delay) {
         Thread restartThread = new Thread(() -> {
             log.info("Restarting ACSP Discord Bot...");
             context.close();
