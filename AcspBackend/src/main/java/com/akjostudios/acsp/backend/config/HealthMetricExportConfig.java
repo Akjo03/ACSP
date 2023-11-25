b700ba177682e314642b4e074d4dbf8b3b3af0a1
Lukas Küffer <lukas.kueffer@outlook.com>
2023-11-25 18:25:47 +0100

    Made reactor use virtual threads and resolved circular dependency issues with the HealthMetricExportConfig

--- AcspBackend/src/main/java/com/akjostudios/acsp/backend/config/HealthMetricExportConfig.java
+++ AcspBackend/src/main/java/com/akjostudios/acsp/backend/config/HealthMetricExportConfig.java
@@ -3,23 +3,35 @@ package com.akjostudios.acsp.backend.config;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tags;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.actuate.health.HealthContributorRegistry;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 @Configuration(proxyBeanMethods = false)
 public class HealthMetricExportConfig {
-    @Bean
-    public MeterRegistryCustomizer<MeterRegistry> healthRegistryCustomizer(@NotNull HealthContributorRegistry healthRegistry) {
-        return registry -> healthRegistry.stream().forEach(
-            healthContributor -> registry
-                .gauge("health", Tags.of("name", healthContributor.getName()), healthRegistry, health -> getStatusCode(
+
+    @Bean // Could be more concise using a MeterRegistryCustomizer and HealthContributorRegistry but that can create a circular dependency issue
+    public ApplicationListener<ContextRefreshedEvent> healthMetricSetupListener(@NotNull ApplicationContext context) {
+        return event -> {
+            HealthContributorRegistry healthRegistry = context.getBean(HealthContributorRegistry.class);
+            MeterRegistry meterRegistry = context.getBean(MeterRegistry.class);
+
+            healthRegistry.stream().forEach(
+                healthContributor -> meterRegistry.gauge(
+                    "health",
+                    Tags.of("name", healthContributor.getName()),
+                    healthRegistry,
+                    health -> getStatusCode(
                         ((HealthIndicator) health.getContributor(healthContributor.getName())).getHealth(false).getStatus()
-                ))
-        );
+                    )
+                )
+            );
+        };
     }
 
     private int getStatusCode(@NotNull Status status) {
